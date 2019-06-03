faacd997f94b6e93b920590584f927084a24f2b2
nerik <erik.escoffier@gmail.com>
2019-06-03 16:50:02 +0200

    clean up pbf folders before regenerating

--- scripts/chile.sh
+++ scripts/chile.sh
@@ -3,9 +3,11 @@
 # transporters
 tippecanoe data/chile/transporters.csv -o data/chile/transporters.mbtiles --force --layer=chile_transporters --no-tile-compression --maximum-zoom=12 --drop-densest-as-needed
 
+rm -rf data/chile/transporters
 mb-util --image_format=pbf data/chile/transporters.mbtiles data/chile/transporters
 
 # aquaculture
 tippecanoe data/chile/aquaculture.csv -o data/chile/aquaculture.mbtiles --force --layer=chile_aquaculture --no-tile-compression --maximum-zoom=12 --drop-densest-as-needed
 
+rm -rf data/chile/aquaculture
 mb-util --image_format=pbf data/chile/aquaculture.mbtiles data/chile/aquaculture
