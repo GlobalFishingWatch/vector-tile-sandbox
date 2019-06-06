47696ef81d81e136d7e5cbd658d143f033c41c96
nerik <erik.escoffier@gmail.com>
2019-06-06 15:06:08 +0200

    allow inspecting remote tiles

--- scripts/inspect-vector-tile.js
+++ scripts/inspect-vector-tile.js
@@ -4,6 +4,7 @@ const Pbf = require('pbf')
 
 const VectorTile = require('@mapbox/vector-tile').VectorTile
 
+const request = require("request");
 const fs = require('fs')
 
 const tilePath = process.argv[2]
@@ -13,20 +14,40 @@ if (tilePath === undefined) {
   process.exit(1)
 }
 
-console.log('Reading tile:', tilePath)
+const readTile = (tile) => {
+  console.log('Reading tile:', tilePath)
 
-const pbf = new Pbf(fs.readFileSync(tilePath))
-const vectorTile = new VectorTile(pbf)
-console.log('pbf length:', pbf.length)
+  const pbf = new Pbf(tile)
+  const vectorTile = new VectorTile(pbf)
+  console.log('pbf length:', pbf.length)
+  
+  const layers = vectorTile.layers
+  const mainLayerName = Object.keys(layers)[0]
+  console.log('layer name:', mainLayerName)
+  const mainLayer = layers[mainLayerName]
+  console.log('layer length', mainLayer.length)
+  const aFeature = mainLayer.feature(0)
+  console.log('example feature type:', aFeature.type)
+  console.log('example feature:', aFeature.properties)
+  console.log('example feature:bbox:', aFeature.bbox())
+  console.log('example feature to GeoJSON')
+  console.log( aFeature.toGeoJSON(5,26,16))
+}
 
-const layers = vectorTile.layers
-const mainLayerName = Object.keys(layers)[0]
-console.log('layer name:', mainLayerName)
-const mainLayer = layers[mainLayerName]
-console.log('layer length', mainLayer.length)
-const aFeature = mainLayer.feature(0)
-console.log('example feature type:', aFeature.type)
-console.log('example feature:', aFeature.properties)
-console.log('example feature:bbox:', aFeature.bbox())
-console.log('example feature to GeoJSON')
-console.log( aFeature.toGeoJSON(5,26,16))
+if (tilePath.substr(0, 4) === 'http') {
+  var requestSettings = {
+    method: 'GET',
+    url: tilePath,
+    encoding: null,
+};
+  request(requestSettings, (error, response, body) => {
+    // let json = JSON.parse(body);
+    console.log(body);
+    readTile(body)
+  });
+  return
+} else {
+  fs.readFileSync(tilePath)
+}
+
+
